ebacd31ea6f30b6f70a4b2b65f42b050855f69ee
Peter Knipp <peteraknipp@gmail.com>
2021-06-04 06:54:46 -0400

    Create models/game.js

--- TODO.md
+++ TODO.md
@@ -3,6 +3,17 @@ shift photo-stage from Signup to EditUser
 improve auth error-handling (on front AND back?)
 put validation stuff in several columns of Users table
 DRY up code associated with multiplicity of columns in Users table
+consolidate Account and Signup components
+change cell DataTypes from INTEGER to BIGINT
+in model files insert associations between tables
 
 use React native?
 generalize to handle multiple sports (ie, not just volleyball)
+
+Aaron: modify DrawSQL schema to reflect any changes:
+    skillLevel -> skill
+    nickName in Users table
+    gameOwner -> ownerId
+    date & time -> dateTime
+    Signup table -> ??? (Reservations, Enrollments, etc)
+    extraInformation -> extraInfo
--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 import Logout from "./admin/Logout";
 import Feature1 from "./feature1/index";
 import Account from "./admin/Account";
-import Signup from "./admin/Signup";
+import SignUp from "./admin/SignUp";
 import Home from "./admin/Home";
 import { store } from "../index";
 
@@ -31,7 +31,7 @@ class Container extends React.Component {
           <Route path="/logout"    component={Logout}   />
           <Route path="/feature1"      component={Feature1}     />
           <Route path="/account"   component={Account} />
-          <Route path="/manageuser"   render={() => <Signup update={true}/>}/>
+          <Route path="/manageuser"   render={() => <SignUp update={true}/>}/>
           <Route path="/"          component={Home} />
         </Switch>
       </BrowserRouter>
