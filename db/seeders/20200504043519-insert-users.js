5fe99dd8be7f62b32e5454545c13e9a8db4d079d
Peter Knipp <peteraknipp@gmail.com>
2021-06-03 21:30:21 -0400

    Flesh out remaining columns of Users table, and modify Signup & Account components, and authentication.js

--- db/seeders/20200504043519-insert-users.js
+++ db/seeders/20200504043519-insert-users.js
@@ -15,8 +15,26 @@ function r(o) {
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
-      r({email:'volleyb@aol.com',hashedPassword:await bcrypt.hash('password',10)}),
-      r({email: 'jdoe@aol.com', hashedPassword:await bcrypt.hash('password', 10)}),
+      r({
+        email: 'volleyb@aol.com',
+        nickName: 'Volley B',
+        firstName: 'Volley',
+        lastName: 'Ball',
+        cell: 1234567890,
+        skill: 7,
+        photo: "Volley B's photoURL",
+        hashedPassword: await bcrypt.hash('password',10)
+      }),
+      r({
+        email: 'jdoe@aol.com',
+        nickName: 'John D',
+        firstName: 'John',
+        lastName: 'Doe',
+        cell: 1345678901,
+        skill: 4,
+        photo: "John D's photoURL",
+        hashedPassword: await bcrypt.hash('password',10)
+      }),
     ]);
   },
 
