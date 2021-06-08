015e2d18f6231b1e220a9f10b94a964b9ad27b25
Peter Knipp <peteraknipp@gmail.com>
2021-06-07 22:12:18 -0400

    Tighten up the model, migration, and seed files.

--- db/models/game.js
+++ db/models/game.js
@@ -4,27 +4,13 @@ module.exports = (sequelize, DataTypes) => {
   const Game = sequelize.define(
     "Game",
     {
-      ownerId: {
-        allowNull: false,
-        type: DataTypes.INTEGER,
-      },
-      address: {
-        allowNull: false,
-        type: DataTypes.TEXT,
-      },
-      dateTime: {
-        allowNull: false,
-        type: DataTypes.DATE,
-      },
-      minSkill: {
-        type: DataTypes.INTEGER,
-      },
-      maxSkill: {
-        type: DataTypes.INTEGER,
-      },
-      extraInfo: {
-        type: DataTypes.TEXT,
-      }
+      ...['ownerId', 'minSkill', 'maxSkill'].reduce((pojo, key) => {
+        return ({...pojo, [key]: {allowNull: false, type: DataTypes.INTEGER}});
+      }, []),
+      ...['address', 'extraInfo'].reduce((pojo, key) => {
+        return ({...pojo, {[key]: allowNull: false, type: DataTypes.TEXT}});
+      }, []),
+      dateTime: {allowNull: false, type: DataTypes.DATE}
     },
   );
 
