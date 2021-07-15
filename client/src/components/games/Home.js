5cd4d057326d47c418bca3ca5258ac69787d31e5
Peter Knipp <peteraknipp@gmail.com>
2021-07-14 23:07:33 -0400

    Fully implement bootstrap for tables.

--- client/src/components/games/Home.js
+++ client/src/components/games/Home.js
@@ -3,17 +3,14 @@ import { NavLink } from "react-router-dom";
 import BootstrapTable from 'react-bootstrap-table-next';
 
 import AuthContext from '../../auth';
-import Game from './Game';
 
 const Home = () => {
-    const { currentUser, rerender } = useContext(AuthContext);
-    const [games, setGames] = useState([]);
-    const [gamesOwned, setGamesOwned] = useState([]);
-    const [gamesReserved, setGamesReserved] = useState([]);
-    const [gamesOther, setGamesOther] = useState([]);
-    const [message, setMessage] = useState('');
-
-    const keys = [
+    const options = [
+        ["games organized by you", "Edit game details"],
+        ["games for which you are registered to play", "Edit reservation"],
+        ["games for which you are not registered to play", "Make reservation"]
+    ];
+    let allKeys = [
         ['date', 'Game date'],
         ['time', 'Start time'],
         ['address', 'Address'],
@@ -22,19 +19,23 @@ const Home = () => {
         ['owner', 'Game organizer'],
         ['edit', '']
     ];
-    // let newGames = keys.reduce((owned, key) => ({...owned, [key[0]]: []}), {});
-    const columns = keys.map(key => ({dataField: key[0], text: key[1], sort: true}));
+    const { currentUser } = useContext(AuthContext);
+    const [allGames, setAllGames] = useState([]);
+    const [games, setGames] = useState([]);
+    const [keys, setKeys] = useState([...allKeys]);
+    const [selectedOption, setSelectedOption] = useState(0);
+    const [message, setMessage] = useState('');
 
     useEffect(() => {
         (async () => {
             const response = await fetch(`/api/games`);
             let data = await response.json();
             if (response.ok) {
-                let newGames = [];
+                let newAllGames = [];
                 data.games.forEach(game => {
-                    let {id, dateTime, address, duration, count, owner} = game;
-                    let newGame = {id, address, count, owner: owner.nickName}
-                    let [date, time] = game.dateTime.split("T");
+                    let {id, dateTime, address, duration, count, owner, reservationId} = game;
+                    let newGame = {id, address, count, reservationId, owner: owner.nickName}
+                    let [date, time] = dateTime.split("T");
                     time = time.slice(0, 5);
                     let minutes = Math.round(duration.value / 60);
                     let hours = Math.floor(minutes / 60);
@@ -42,82 +43,76 @@ const Home = () => {
                     minutes -= hours * 60;
                     minutes = (!minutes ? "00" : minutes < 10 ? "0" : "") + minutes;
                     duration = hours + ":" + minutes;
-                    newGame = {...newGame, date, time, duration};
-                    newGame.edit = (
-                        <NavLink
-                            exact to={`/games/${id}`}
-                            className="nav"
-                            activeClassName="active"
-                        >
-                            Edit game details
-                        </NavLink>
-                    )
-                    newGames.push(newGame);
-                })
-                setGames(newGames);
-              setGamesOwned(data.games.filter(game => {
-                  return game.ownerId === currentUser.id;
-              }).sort((game1, game2) => new Date(game1.dateTime) - new Date(game2.dateTime)));
-              setGamesReserved(data.games.filter(game => {
-                  return !!game.reservationId;
-              }).sort((game1, game2) => game1.owner.nickName.localeCompare(game2.owner.nickName)));
-              setGamesOther(data.games.filter(game => {
-                  return !game.reservationId;
-              }).sort((game1, game2) => game1.duration.value - game2.duration.value));
+                    newGame = {...newGame, date, time, duration, ownerId: owner.id};
+                    newGame.ownerId = owner.id;
+                    newAllGames.push(newGame);
+                });
+                setAllGames(newAllGames);
             } else {
-              setMessage(data.message || data.error.errors[0]);
+                setMessage(data.message || data.error.errors[0]);
             }
         })()
-    }, [rerender, currentUser.id]);
+    }, [currentUser.id]);
 
+    useEffect(() => {
+        setKeys(allKeys.filter(key => selectedOption ? true : key[0] !== 'owner'));
+        let bool;
+        let newGames = allGames.filter((game, i) => {
+            if (selectedOption) {
+                bool = (!(selectedOption - 1) !== !game.reservationId);
+            } else {
+                bool = (game.ownerId === currentUser.id);
+            }
+            return bool;
+        });
+        newGames.forEach(game => {
+            let path;
+            if (selectedOption) {
+                path = `/reservations/${game.reservationId}-${game.id}`;
+            } else {
+                path = `/games/${game.id}`;
+            }
+            game.edit = (
+                <NavLink
+                    exact to={path}
+                    className="nav"
+                    activeClassName="active"
+                >
+                    {options[selectedOption][1]}
+                </NavLink>
+            )
+        });
+        setGames(newGames);
+    }, [allGames, selectedOption]);
 
+    const columns = keys.map(key => ({dataField: key[0], text: key[1], sort: true}));
 
     return (
         <>
-            <div>
-                <div className="welcome">
-                    <p>Here goes a welcome message.</p>
+            <div className="welcome">
+                <p>Here goes a welcome message.</p>
+            </div>
+            {options.map((option, i) => (
+                <>
+                    <input
+                        key={i}
+                        type="radio"
+                        value={i}
+                        checked={i === selectedOption}
+                        onChange={e => setSelectedOption(Number(e.target.value))}
+                    />
+                    <span>{option[0]}</span>
+                </>
+            ))}
+            {selectedOption ? null :
+                <div>
+                    <NavLink exact to={"/games/0"} className="nav" activeClassName="active">
+                        <div>Create new Game</div>
+                    </NavLink>
                 </div>
-            </div>
-            <div>
-                <NavLink exact to={"/games/0"} className="nav" activeClassName="active">
-                    Create new Game
-                </NavLink>
-            </div>
+            }
             <BootstrapTable keyField='id' data={ games } columns={ columns } />
-
-            {!gamesOwned.length ? null :
-                <div>
-                    Game{gamesOwned.length > 1 ? "s" : ""} owned by you (sorted    chronologically):
-                    <ul>
-                        {gamesOwned.map(game => {
-                        return <Game key={game.id} game={game} type={"Edit game"}/>;
-                        })}
-                    </ul>
-                </div>
-            }
-            {!gamesReserved.length ? null :
-                <div>
-                    Game{gamesReserved.length > 1 ? "s" : ""} for which you have a reservation (sorted by game owner):
-                    <ul>
-                        {gamesReserved.map(game => {
-                            return <Game key={game.id} type={"Edit reservation"} game={game} />;
-                        })}
-                    </ul>
-                </div>
-            }
-            {!gamesOther.length ? null :
-                <div>
-                    Game{gamesOther.length > 1 ? "s" : ""} for which you have not yetmade a reservation (sorted geographically):
-                    <ul>
-                        {gamesOther.map(game => {
-                            return <Game key={game.id} game={game} type={"Makereservation"}    />;
-                        })}
-                    </ul>
-                </div>
-            }
         </>
     )
 }
-
 export default Home;
