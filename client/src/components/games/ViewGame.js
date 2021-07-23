aa0f7190490e567f4b1042eaa74621e19c35e113
Peter Knipp <peteraknipp@gmail.com>
2021-07-23 08:04:19 -0400

    Make each row of report a single-line, reduce width of boolean columns to one character.

--- client/src/components/games/ViewGame.js
+++ client/src/components/games/ViewGame.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 import BootstrapTable from 'react-bootstrap-table-next';
 
 import AuthContext from '../../auth';
+import { time } from 'faker';
 
 const ViewGame = ({ match }) => {
   const { fetchWithCSRF, rerender, setRerender } = useContext(AuthContext);
@@ -19,18 +20,40 @@ const ViewGame = ({ match }) => {
   const [errors, setErrors] = useState([]);
 
   let history = useHistory();
+  const columns2 = [
+    ['nickName', 'Name'],
+    ['photo', ''],
+    ['email', 'Email'],
+    ['createdAt', `Member\n since`],
+    ['updatedAt', `Reservation\n date/time`],
+    ['skill', 'Skill'],
+    ['setter', 'S'],
+    ['middle', 'M'],
+    ['rightSide', 'R'],
+    ['outside', 'O'],
+    ['libero', 'L'],
+    ['twos', '2'],
+    ['fours', '4'],
+    ['sixes', '6']
+  ];
 
   useEffect(() => {
     (async() => {
         const res = await fetch(`/api/games/${game.id}`);
         let newGame = (await res.json()).game;
         let newPlayers = newGame.players;
-        let newColumns = Object.entries(newPlayers[0]).sort((a, b) => typeof(a[1]) < typeof(b[1]) ? 1 : typeof(a[1]) > typeof(b[1]) ? -1 : 0).map(([key]) => ({dataField: key, text: key, sort: true})).filter(key => key.text !== 'id');
-        setColumns(newColumns);
+        // let newColumns = Object.entries(newPlayers[0]).sort((a, b) => typeof(a[1]) < typeof(b[1]) ? 1 : typeof(a[1]) > typeof(b[1]) ? -1 : 0).map(([key]) => ({dataField: key, text: key, sort: true})).filter(key => key.text !== 'id');
+        setColumns(columns2.map(pair => ({dataField: pair[0], text: pair[1], sort: true})));
+        // including prop style: {'white-space': 'nowrap'} did not help
         newPlayers = newPlayers.map(player => {
-          return Object.entries(player).reduce((player, prop) => {
+          player = Object.entries(player).reduce((player, prop) => {
             return {...player, [prop[0]]: prop[1] === true ? "x" : prop[1] === false ? "" : prop[1]};
-          })
+          });
+          player.createdAt = player.createdAt.split('T')[0];
+          let updatedAt = player.updatedAt.split('T');
+          player.updatedAt = updatedAt[0].slice(5) + ' ' + updatedAt[1].slice(0, -8);
+          player.photo = 'photo';
+          return player;
         })
         setPlayers(newPlayers);
         // React does not like null value, which might be stored in db.
