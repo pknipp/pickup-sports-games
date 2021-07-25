38baa5db9747491f5e3b2faa999a03fe3cac6345
Peter Knipp <peteraknipp@gmail.com>
2021-07-25 15:10:52 -0400

    Prepare to change back to master branch.

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './reset.css';
 import './index.css';
-// import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 // import './index.css';
 import App from './App';
 require("dotenv").config();
