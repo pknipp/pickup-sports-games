6ed8c8a1a72d9728b10e7a175942ef78cd7aac1c
Peter Knipp <peteraknipp@gmail.com>
2021-08-27 17:48:16 -0400

    Temporarily reduce boolean length by 4 (ie, removing gender booleans).

--- db/seeders/4-insert-reservations.js
+++ db/seeders/4-insert-reservations.js
@@ -12,7 +12,7 @@ for (let iUser = 0; iUser < numberOfUsers; iUser++) {
       const reservation = {playerId: 1 + iUser, gameId: i + 1};
       // Why is JSON.parse needed in the following?
       let gt = gameTypes[games[i].gameTypeId - 1];
-      let boolsLength = 4 + (gt.positions ? JSON.parse(gt.positions).length : 0) + (gt.sizes ? JSON.parse(gt.sizes).length : 0);
+      let boolsLength = 0 + (gt.positions ? JSON.parse(gt.positions).length : 0) + (gt.sizes ? JSON.parse(gt.sizes).length : 0);
       reservation.bools = Math.floor(Math.random() * 2 ** boolsLength);
       let updatedAt = faker.date.past(0.1);
       [reservation.createdAt, reservation.updatedAt] = [faker.date.past(0.1, updatedAt), updatedAt];
