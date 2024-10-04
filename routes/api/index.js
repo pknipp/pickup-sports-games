67cb8e8816319e13906c3a50a037a87553ce3d44
Peter Knipp <peteraknipp@gmail.com>
2024-10-04 07:35:06 -0400

    Run npm install?

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,4 @@
+
 const router = require('express').Router();
 const routes = ['session', 'users', 'events', 'reservations', 'favorites', 'sports'];
 routes.forEach(route => router.use(`/${route}`, require(`./${route}`)));
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
+
 const router = require('express').Router();
 const api = require('./api');
-
 router.use('/api', api);
-
 module.exports = router;
+//module.exports = require('express').Router().use('/api',require('./api'));
